ba8ed79e203373ccc46bf757ca3db41b9e90efde
easondada <309397311@qq.com>
2015-01-16 11:19:17 +0800

    更改了xml，增加了按钮资源文件，能显示基本窗口，但没有系统菜单

--- WinDui/WinDui/WinDui.cpp
+++ WinDui/WinDui/WinDui.cpp
@@ -19,83 +19,23 @@ using namespace DuiLib;
 #   endif
 #endif
 
-class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+class CDuiFrameWnd : public WindowImplBase
 {
 public:
-	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
-	virtual void    Notify(TNotifyUI& msg) 
-	{//消息处理
-		if (msg.sType == _T("click"))
-		{
-			if (msg.pSender->GetName() == _T("btnHello"))
-			{
-				::MessageBox(NULL, _T("我是按钮"), _T("点击了按钮"), NULL);
-			}
-		}
-	}
-
-	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
-	{
-		LRESULT lRes = 0;
-
-		if (uMsg == WM_CREATE)
-		{
-			m_PaintManager.Init(m_hWnd);
-
-			CDialogBuilder builder;
-			CControlUI* pRoot = builder.Create(_T("WinDui.xml"), (UINT)0, NULL, &m_PaintManager);   // .xml需要放到exe目录下
-			ASSERT(pRoot && "Failed to parse XML");
-
-			m_PaintManager.AttachDialog(pRoot);
-			m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
-			return lRes;
-			/*
-			CControlUI *pWnd = new CButtonUI;
-			//pWnd->SetName(_T("btnHello"));      // 设置控件的名称，这个名称用于标识每一个控件，必须唯一
-			pWnd->SetText(_T("Hello World"));   // 设置文字
-			pWnd->SetBkColor(0xFFFFFFFF);       // 设置背景色
-
-			m_PaintManager.Init(m_hWnd);
-			m_PaintManager.AttachDialog(pWnd);
-			//相当于派发消息
-			m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
-			return lRes;*/
-		}
-		else if (uMsg == WM_NCACTIVATE)
-		{
-			if (!::IsIconic(m_hWnd))
-			{
-				return (wParam == 0) ? TRUE : FALSE;
-			}
-		}
-		else if (uMsg == WM_NCCALCSIZE)
-		{
-			return 0;
-		}
-		else if (uMsg == WM_NCPAINT)
-		{
-			return 0;
-		}
-
-		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
-		{
-			return lRes;
-		}
-
-		return __super::HandleMessage(uMsg, wParam, lParam);
-	}
-
-protected:
-	CPaintManagerUI m_PaintManager;
+	virtual LPCTSTR    GetWindowClassName() const   { return _T("DUIMainFrame"); }
+	virtual CDuiString GetSkinFile()                { return _T("WinDui.xml"); }
+	virtual CDuiString GetSkinFolder()              { return _T(""); }
 };
 
 int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
 {
 	CPaintManagerUI::SetInstance(hInstance);
-	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());   // 设置资源的默认路径（此处设置为和exe在同一目录）
+
 	CDuiFrameWnd duiFrame;
 	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
 	duiFrame.CenterWindow();
 	duiFrame.ShowModal();
 	return 0;
 }
+
+
